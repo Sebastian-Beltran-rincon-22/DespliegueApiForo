96e2fc498d5bc150738744ce0f0631b03834043e
sewastian <sebastianrincon834@gmail.com>
2023-08-26 19:50:39 -0500

    update [controllers -> user.js]

--- controllers/user.js
+++ controllers/user.js
@@ -37,7 +37,7 @@ const controllerUser = {
     getUser: async (req, res) => {
         try {
             const users = await User.find({})
-            res.json(users)
+            res.json(users.reverse())
         } catch (error) {
             return res.status(500).json({ msg: error })
         }
