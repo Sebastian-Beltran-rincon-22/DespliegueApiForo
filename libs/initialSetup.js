2f6c9be1a492c2a8b2a566a37bc9cfc78b033bcf
sewastian <sebastianrincon834@gmail.com>
2023-09-10 11:43:16 -0500

    feat [libs -> initialSetup.js]

--- libs/initialSetup.js
+++ libs/initialSetup.js
@@ -21,25 +21,31 @@ const rolAdmin ={
     }
 },
 
-    adminprint : async () =>{
+    adminprint : async () => {
+        try {
 
-        const userFound = await  User.findOne({email: config.ADMIN_EMAIL})
-        console.log(userFound)
-        if (userFound) return
 
-        const admin = await Admin.find({name: {$in:["admin"]}})
+            const userFound = await User.findOne({ email: config.ADMIN_EMAIL });
+            console.log(userFound);
 
-        //Crear el primer Administrador
-        const userRegis = await User.create({
-            userName: config.ADMIN_USERNAME,
-            email: config.ADMIN_EMAIL,
-            password: config.ADMIN_PASSWORD,
-            admin: admin.map((admins) =>admins._id)
-        })
+            if (userFound) return;
 
-        console.log(`new user created: ${userRegis.email}`)
+            
+            const admin = await Admin.find({name: {$in:["admin"]}})
+
+            const userRegis = await User.create({
+                userName: config.ADMIN_USERNAME,
+                email: config.ADMIN_EMAIL,
+                password: config.ADMIN_PASSWORD,
+                admin: admin.map((admins) =>admins._id)
+            });
+
+            console.log(`New user created: ${userRegis.email}`);
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
 
-module.exports = rolAdmin.createAdmin
+module.exports = rolAdmin
 
