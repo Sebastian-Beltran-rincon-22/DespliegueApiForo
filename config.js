26d6cd66d5689bac72c9c611484b32cdc4f70784
sewastian <sebastianrincon834@gmail.com>
2023-09-21 12:13:07 -0500

    update [config.js]

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports ={
     SECRET: "poofo123secretclave456",
 
-    ADMIN_EMAIL : "sebastianrincon834@gmail.com",
+    ADMIN_EMAIL : "johanrincon404@gmail.com",
 
     ADMIN_USERNAME :"SebastianB",
 
-    ADMIN_PASSWORD : "admin123",
+    ADMIN_PASSWORD : "S22122002e.",
 
     URL: "https://c9-pacto-front.vercel.app/" //URL = http://localhost:3000
 
